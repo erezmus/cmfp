041fd3bb814d0fb9ddf03998bac4c440c110df2c
Erez Odier <2045191+erezmus@users.noreply.github.com>
2024-11-18 10:55:25 +0000

    feat: add missing translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,7 +5,7 @@ export const languages = {
 
 export const defaultLang = "en";
 
-export const ui = {
+export const ui: Record<keyof typeof languages, Record<string, string>> = {
   en: {
     "menu.home": "Home",
     "menu.about": "About",
@@ -13,6 +13,8 @@ export const ui = {
     "menu.resources": "Resources",
     "menu.teacher_guide": "Teacher Guide",
     "menu.faq": "FAQ",
+    "logo.tagline": "Capoeira movement for Palestine",
+    "header.add_school": "Add your school"
   },
   "pt-br": {
     "menu.home": "Início",
@@ -21,5 +23,7 @@ export const ui = {
     "menu.resources": "Recursos",
     "menu.teacher_guide": "Guia pra Professor",
     "menu.faq": "Perguntas frequentes",
+    "logo.tagline": "Movimento de Capoeira pela Palestina",
+    "header.add_school": "Adicione sua escola"
   },
 } as const;
